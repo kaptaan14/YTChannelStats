adfe6bd87ac2c2de1378177c0b328387fa6d9da1
Ashish Singh <118279121+kaptaan14@users.noreply.github.com>
2024-08-22 09:24:41 +0530

    done some changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  build: {
+    outDir: 'dist' 
+  },
   plugins: [react()],
 });
